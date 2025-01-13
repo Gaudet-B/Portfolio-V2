417267812244d4a07f7b0f48ac9d3a7f59a8b423
Brian Gaudet <brian.f.gaudet@gmail.com>
2025-01-13 12:40:51 -0800

    added graphql

--- server/server.js
+++ server/server.js
@@ -1,22 +1,31 @@
 const cors = require('cors')
 const express = require('express')
 const cookieParser = require('cookie-parser')
-const nodemailer = require("nodemailer")
-// const bodyParser = require('body-parser')
-
-const fs = require('fs')
-const path = require('path')
+const nodemailer = require('nodemailer')
+const { createHandler } = require('graphql-http/lib/use/express')
+const { buildSchema } = require('graphql')
 
 const app = express()
+
+const schema = buildSchema(`
+    type Query {
+        hello: String
+    }
+`)
+
+const root = {
+  hello: () => 'Hello World',
+}
+
+const handler = createHandler({ schema, rootValue: root })
+
+app.use('/graphql', handler)
 
 require('./config/mongoose.config')
 require('dotenv').config()
 
-
 app.use(express.json())
 app.use(cookieParser())
-// app.use(bodyParser.urlencoded({ extended: false }))
-// app.use(bodyParser.json())
 
 // CRA App
 // app.use(cors({ credentials: true, origin: 'http://localhost:3000' }))
@@ -33,19 +42,19 @@ const userName = process.env.USER_SECRET_KEY
 const passWord = process.env.PASS_SECRET_KEY
 
 const contactEmail = nodemailer.createTransport({
-    service: 'gmail',
-    auth: {
-        user: userName,
-        pass: passWord,
-    },
+  service: 'gmail',
+  auth: {
+    user: userName,
+    pass: passWord,
+  },
 })
 
 contactEmail.verify((error) => {
-    if (error) {
-        console.log(error);
-    } else {
-        console.log("Ready to Send");
-    }
+  if (error) {
+    console.log(error)
+  } else {
+    console.log('Ready to Send')
+  }
 })
 
 app.listen(port, () => console.log(`Ready to rock on port ${port}`))
